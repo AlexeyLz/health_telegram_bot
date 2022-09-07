0e7668ba54448ea000cfdfab0c19acb7a239c1f9
Alexey Lazarev <52629691+AlexeyLz@users.noreply.github.com>
2022-09-07 16:30:53 +0300

    test tokens

--- bot_settings.py
+++ bot_settings.py
@@ -8,14 +8,15 @@ import psycopg2
 
 load_dotenv(find_dotenv())
 
-TOKEN = os.getenv('TOKEN')
-
+#TOKEN = os.getenv('TOKEN')
+TOKEN = '5403558275:AAE-oc5a_28rwwUfPwPfPqeEJa2q1dE7yrg'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 path_to_main_gif = 'resources/start_gif.gif'
 
-path_to_db_bot = os.getenv('path_to_db_bot')
+#path_to_db_bot = os.getenv('path_to_db_bot')
+path_to_db_bot = 'postgres://fmmpoivj:U939QIpG1RAQj4C-IqHlIt7ai7Tx3B0O@abul.db.elephantsql.com/fmmpoivj'
 up.uses_netloc.append("postgres")
 url = up.urlparse(path_to_db_bot)
 connection = psycopg2.connect(database=url.path[1:],
